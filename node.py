8314fc289f5a489d173f5db88275a7d0e7ec30d6
amirfazel <amirfazel45@gmail.com>
2022-02-03 20:14:33 +0330

    chain route added

--- node.py
+++ node.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 
 from wallet import Wallet
+from blockchain import Blockchain
+
 
 web_app = Flask(__name__)
 wallet =  Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(web_app)
 
 
 @web_app.route('/', methods=['GET'])
 def get_ui():
     return 'this works!'
+
+@web_app.route('/chain',methods=['GET'])
+def get_chain():
+    chain = blockchain.chain
+    chain_to_dict = [block.__dict__.copy() for block in chain]
+    for dict_block in chain_to_dict:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(chain_to_dict), 200
 
 if __name__ == '__main__':
     web_app.run(host='0.0.0.0', port=5000)
